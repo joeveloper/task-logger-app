51732980c55e3970443f89838efc1a27ef3dcee4
joeveloper <bebosir@gmail.com>
2022-08-19 15:48:47 +0100

    fetch issues

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,39 +1,39 @@
 import React, {useState, useEffect} from 'react';
+import LogItem from './LogItem';
+import 'db.json';
 
 const Logs = () => {
     const [logs, setLogs] = useState([]);
     const [loading, setLoading] = useState(false);
 
+   
     useEffect(() => {
         getLogs();
-        //eslint-disble-next-line
+        
     }, []);
-
+    
     const getLogs = async () => {
-        setLoading(true);
+        setLoading(true)
         const res = await fetch('/logs');
-        const data = await res.json();
-
-        console.log(data)
-
+        const data = await res.json();   
         setLogs(data);
-        setLoading(false)
+        setLoading(false);
     }
 
     if (loading) {
-        return <h4>Loading... </h4>
+         return <h4>Loading... </h4>
     };
 
   return (
     <ul className="collection-with-header">
-        <li className='collection-header'>
+        <li className="collection-header">
         <h4 className="center">System logs</h4>
         </li>
-        {!loading && logs.length === 0 ? (<p className='center'>No logs to show ...</p>) : (
-            logs.map(log => {
-                <li>{log.message}</li>
-            })
-        )} 
+        {!loading && logs.length === 0 ?  (<p className="center">No logs to show ...</p>) : console.log(logs) && (
+            logs.map((log) => 
+                 (<LogItem log={log} key={log.id}/>) 
+            )
+        )};
     </ul>
   )
 }
