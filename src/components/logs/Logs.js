0b7bb291040fa4c8c9071bef80d6a54a6fc94be2
Network <Network@Mohammads-MacBook-Pro.local>
2022-12-14 13:16:06 +0100

    updates

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -6,50 +6,39 @@ import PropTypes from 'prop-types';
 import { getLogs } from '../../actions/logActions';
 
 const Logs = ({ log: { logs, loading }, getLogs }) => {
-    // const [logs, setLogs] = useState([]);
-    // const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    getLogs();
+    // eslint-disable-next-line
+  }, []);
 
-    useEffect(() => {
-        getLogs();
-        //eslint-disable-next-line 
-    }, []);
+  if (loading || logs === null) {
+    return <Preloader />;
+  }
 
-    // const getLogs = async () => {
-    //     setLoading(true);
-    //     const res = await fetch('/logs');
-    //     const data = await res.json();
-        
-    //     setLogs(data);
-    //     setLoading(false)
-    // }
-
-    if (loading || logs === null) {
-        return <Preloader />
-    };
-
-    return (
-        <ul className="collection with-header">
-            <li className="collection-header">
-                <h4 className="center">System logs</h4>
-            </li>
-            {!loading && logs.length === 0 ? (
-            <p className="center">No logs to show ...</p>
-            ) : (
-                logs.map((log) =>
-                    (<LogItem log={log} key={log.id} />)
-                )
-            )}
-        </ul>
-    )
+  return (
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to show...</p>
+      ) : (
+        logs.map(log => <LogItem log={log} key={log.id} />)
+      )}
+    </ul>
+  );
 };
 
 Logs.propTypes = {
-    log: PropTypes.object.isRequired
-}
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
+};
 
-// get data from state
 const mapStateToProps = state => ({
-    log: state.log
-})
+  log: state.log
+});
 
-export default connect(mapStateToProps, { getLogs })(Logs)
+export default connect(
+  mapStateToProps,
+  { getLogs }
+)(Logs);
